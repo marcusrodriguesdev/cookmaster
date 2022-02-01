e42d7c3633214ba4dd817304c436af649458fdff
marcusrodriguesdev <marcus.rodrigues10@hotmail.com>
2022-02-01 20:47:05 -0300

    create user login on controller layer

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -9,6 +9,16 @@ const registerUser = async (_req, res) => {
   return res.status(201).json({ user });
 };
 
+const loginUser = async (_req, res) => {
+  const { email, password } = _req.body;
+  const user = await Service.userLoginService({ email, password });
+
+  if (user.err) return res.status(user.status).json({ message: user.err.message });
+
+  return res.status(200).json(user);
+};
+
 module.exports = {
   registerUser,
+  loginUser,
 };
