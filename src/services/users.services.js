a43e1f19cddff02fdf8316ef5ed781f8bbdd7c10
marcusrodriguesdev <marcus.rodrigues10@hotmail.com>
2022-02-01 20:45:38 -0300

    create user login service in service layer

--- src/services/users.services.js
+++ src/services/users.services.js
@@ -1,11 +1,15 @@
-const { validateUserData } = require('../middlewares/validateRegister');
+const jwt = require('jsonwebtoken');
+const {
+  validateUserRegistrationData, validateUserLoginData,
+} = require('../middlewares/validateUser');
+require('dotenv').config();
 const Model = require('../models/users.models');
 
 const userRegistrationServices = async (dataUser) => {
   const { name, email, password } = dataUser;
   const role = 'user';
 
-  const { error } = validateUserData.validate({ name, email, password });
+  const { error } = validateUserRegistrationData.validate({ name, email, password });
   if (error) return { status: 400, err: { message: 'Invalid entries. Try again' } };
 
   const alreadyExists = await Model.findUserByEmail(email);
@@ -20,6 +24,27 @@ const userRegistrationServices = async (dataUser) => {
   };
 };
 
+const userLoginService = async (dataUser) => {
+  const { email, password } = dataUser;
+
+  const { error } = validateUserLoginData.validate({ email, password });
+  if (error) return { status: 401, err: { message: 'All fields must be filled' } };
+
+  const user = await Model.findUserByEmail(email);
+  if (!user || user.password !== password) {
+    return { status: 401, err: { message: 'Incorrect username or password' } };
+  }
+
+  const { _id, role } = user;
+  const payload = { id: _id, email, role };
+  const token = jwt.sign(payload, process.env.SENHA, {
+    expiresIn: '1d',
+  });
+
+  return { token };
+};
+
 module.exports = {
   userRegistrationServices,
+  userLoginService,
 };
